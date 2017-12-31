f1d0e74173f576b6b6108a8bd6dbc58c2d170718
Pagey <dan.page91@gmail.com>
2017-12-31 15:31:01 +0000

    More detailed styling.

---
+++ scripts.js
@@ -0,0 +1,7 @@
+const numbers = document.getElementsByClassName("number-button");
+
+for (var i = 0; i < numbers.length; i++) {
+  console.log(numbers[i]);
+}
+
+// function to change innerHTML of display
