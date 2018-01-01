7902f61c1776c981d1927de68cd4aedb55cd7221
Pagey <dan.page91@gmail.com>
2018-01-01 08:43:35 +0000

    Array of buttons, array of values.

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
-const numbers = document.getElementsByClassName("number-button");
-
-for (var i = 0; i < numbers.length; i++) {
-  console.log(numbers[i]);
-}
-
-// function to change innerHTML of display
+const display = document.getElementsByClassName("display");
+let numBtns = document.getElementsByClassName("number-button");
+numBtns = Array.from(numBtns);
+const numbers = numBtns.map(numBtn => numBtn.innerHTML);
+// 
+// 1) Create function to add event listener to one button
+// 2) Loop through array to add event listener to all buttons
