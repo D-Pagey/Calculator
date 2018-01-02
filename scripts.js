39d0127a5d38560fc350cc7f63c795dbae4c35b3
D-Pagey <dan.page91@gmail.com>
2018-01-02 22:51:39 +0200

    Assigned values and event listeners to number buttons.

--- scripts.js
+++ scripts.js
@@ -1,7 +1,24 @@
 const display = document.getElementsByClassName("display");
 let numBtns = document.getElementsByClassName("number-button");
 numBtns = Array.from(numBtns);
-const numbers = numBtns.map(numBtn => numBtn.innerHTML);
-// 
-// 1) Create function to add event listener to one button
-// 2) Loop through array to add event listener to all buttons
+
+let operand1 = "";
+let operand2 = "";
+
+function test(x) {
+  operand1 += x.getAttribute("data-value");
+  console.log(operand1);
+};
+
+for (var i = 0; i < numBtns.length; i++) {
+  numBtns[i].addEventListener("click", function(e) {
+    test(e.target);
+  });
+}
+
+/* problems
+
+1) how to pass argument to event listener function
+2) can pass index within function to new function? e.g. test().
+
+*/
